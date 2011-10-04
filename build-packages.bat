b90e4362b808981d151fb7240161cbd675be2d7e
Haacked <haacked@gmail.com>
2011-10-04 14:58:06 -0700

    Updated build script

--- build-packages.bat
+++ build-packages.bat
@@ -1,3 +1,4 @@
+nuget pack src\AspNetHaack\AspNetHaack.csproj -Symbols
 nuget pack src\MvcHaack.ControllerInspector\MvcHaack.ControllerInspector.csproj -Symbols
 nuget pack src\MvcHaack.Ajax\MvcHaack.Ajax.csproj -Symbols
 nuget pack src\MvcHaack.ViewMobilizer\MvcHaack.ViewMobilizer.nuspec -Symbols
