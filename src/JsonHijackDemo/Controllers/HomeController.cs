39fdee0e1e97d414bf7fb002c719dd09da09932c
Haacked <haacked@gmail.com>
2012-04-08 16:18:55 -0700

    Adding JSON Hijacking Demo Project
    This project corresponds to my [blog post on JSON Hijacking](http://haacked.com/archive/2009/06/24/json-hijacking.aspx)

---
+++ src/JsonHijackDemo/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace JsonHijackDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "JSON Hijack Demo";
+
+            return View();
+        }
+
+        [Authorize]
+        public JsonResult AdminBalances()
+        {
+            var balances = new[]
+            {
+                new {Id = 1, Balance = 3.14},
+                new {Id = 2, Balance = 2.72},
+                new {Id = 3, Balance = 1.62}
+            };
+            return Json(balances, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ src/JsonHijackDemo/html/README.md
@@ -0,0 +1,5 @@
+# Security Demos HTML files
+
+The `JsonAttack.html` file needs to be hosted on a separate server in order to demonstrate these security attacks.
+
+Currently, this is hosted at [http://demo.haacked.com/security/JsonAttack.html](http://demo.haacked.com/security/JsonAttack.html)
