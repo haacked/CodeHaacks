2208a888851e67f827fb48bd6f051fcf1fe133fd
Haacked <haacked@gmail.com>
2012-04-08 20:15:23 -0700

    WPF Demo of using Rx to throttle resize events
    This is a sample used to highlight Rx and WPF. Move and resize the window and wait 5 seconds and you'll see the placement of the window get saved.
    This will be featured in [a blog post about Rx](http://haacked.com/archive/2012/04/08/window-placement-rx.aspx)

---
+++ src/WindowPlacementRxDemo/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Windows;
+using ReactiveUI;
+using WindowPlacementExample;
+
+namespace WindowPlacementRxDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Observable.FromEventPattern<SizeChangedEventHandler, SizeChangedEventArgs>
+                (h => SizeChanged += h, h => SizeChanged -= h)
+                .Select(e => EventArgs.Empty).Merge(Observable.FromEventPattern<EventHandler, EventArgs>
+                                                        (h => LocationChanged += h, h => LocationChanged -= h)
+                                                        .Select(e => EventArgs.Empty)
+                ).Throttle(TimeSpan.FromSeconds(5), RxApp.DeferredScheduler)
+                .Subscribe(_ =>
+                {
+                    info.Text = "Placement saved: " + DateTime.Now;
+                    this.SavePlacement();
+                });
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            this.RestorePlacement();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            this.SavePlacement();
+            base.OnClosed(e);
+        }
+    }
+}
