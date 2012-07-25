9ab0ca114062116dee448faad2a0e2ad04a38111
Haacked <haacked@gmail.com>
2012-07-24 21:47:06 -0700

    Updating nuspec and version number

--- src/MvcHaack.ControllerInspector/Properties/AssemblyInfo.cs
+++ src/MvcHaack.ControllerInspector/Properties/AssemblyInfo.cs
@@ -4,8 +4,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("Controller Inspector for ASP.NET MVC 3")]
-[assembly: AssemblyDescription("Adds the ability to inspect metadata about a controller by adding the ?inspect query string.")]
+[assembly:
+    AssemblyDescription("Adds the ability to inspect metadata about a controller by adding the ?inspect query string.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Phil Haack")]
 [assembly: AssemblyProduct("MvcHaack.ControllerInspector")]
@@ -14,9 +16,11 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+
 [assembly: Guid("b287cb00-6351-4c98-b9fa-1403fd47a432")]
 
 // Version information for an assembly consists of the following four values:
@@ -29,5 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
