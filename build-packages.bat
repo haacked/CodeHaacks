0dc8fa7fb55e3417198967e2a1768afa2601305d
Haacked <haacked@gmail.com>
2012-11-13 08:46:55 -0800

    Fixup the MvcHaack.Ajax.v2 project

--- build-packages.bat
+++ build-packages.bat
@@ -1,4 +1,5 @@
 src\.nuget\nuget pack src\AspNetHaack\AspNetHaack.csproj -Symbols
 src\.nuget\nuget pack src\MvcHaack.ControllerInspector\MvcHaack.ControllerInspector.csproj -Symbols
 src\.nuget\nuget pack src\MvcHaack.Ajax\MvcHaack.Ajax.csproj -Symbols
+src\.nuget\nuget pack src\MvcHaack.Ajax.V2\MvcHaack.Ajax.V2.csproj -Symbols
 src\.nuget\nuget pack src\MvcHaack.ViewMobilizer\MvcHaack.ViewMobilizer.nuspec -Symbols
