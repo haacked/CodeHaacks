0d39b48af79b9225d1ca286ac8664c727bef8917
Haacked <haacked@gmail.com>
2013-09-17 10:47:01 -0700

    Add copying attributes code
    This is code from
    http://haacked.com/archive/2010/08/04/copying-attributes.aspx

---
+++ src/MiscUtils/AttributeExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace MiscUtils
+{
+    public static class AttributeExtensions
+    {
+        public static Attribute CreateAttribute(this CustomAttributeData data)
+        {
+            var arguments = from arg in data.ConstructorArguments select arg.Value;
+
+            var attribute = data.Constructor.Invoke(arguments.ToArray()) as Attribute;
+
+            if (data.NamedArguments == null) return attribute;
+
+            foreach (var namedArgument in data.NamedArguments)
+            {
+                var propertyInfo = namedArgument.MemberInfo as PropertyInfo;
+                if (propertyInfo != null)
+                {
+                    propertyInfo.SetValue(attribute, namedArgument.TypedValue.Value, null);
+                }
+                else
+                {
+                    var fieldInfo = namedArgument.MemberInfo as FieldInfo;
+                    if (fieldInfo != null)
+                    {
+                        fieldInfo.SetValue(attribute, namedArgument.TypedValue.Value);
+                    }
+                }
+            }
+
+            return attribute;
+        }
+
+        public static IEnumerable<Attribute> GetCustomAttributesCopy(this Type type)
+        {
+            return CustomAttributeData.GetCustomAttributes(type).CreateAttributes();
+        }
+
+        public static IEnumerable<Attribute> GetCustomAttributesCopy(this Assembly assembly)
+        {
+            return CustomAttributeData.GetCustomAttributes(assembly).CreateAttributes();
+        }
+
+        public static IEnumerable<Attribute> GetCustomAttributesCopy(this MemberInfo memberInfo)
+        {
+            return CustomAttributeData.GetCustomAttributes(memberInfo).CreateAttributes();
+        }
+
+        public static IEnumerable<Attribute> CreateAttributes(this IEnumerable<CustomAttributeData> attributesData)
+        {
+            return from attributeData in attributesData
+                select attributeData.CreateAttribute();
+        }
+    }
+}
---
+++ src/MiscUtilsTests/AttributeExtensionsTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using MiscUtils;
+using Xunit;
+
+namespace MiscUtilsTests
+{
+    public class AttributeExtensionsTests
+    {
+        [Fact]
+        public void CanCopyAttribute()
+        {
+            var attributes = typeof (Character).GetCustomAttributesCopy()
+                .Cast<SomethingAttribute>()
+                .OrderBy(x => x.NamedField)
+                .ToArray();
+
+            Assert.Equal("ROVal1", attributes[0].ReadOnlyProperty);
+            Assert.Equal("NVal1", attributes[0].NamedProperty);
+            Assert.Equal("Val1", attributes[0].NamedField);
+            Assert.Equal("ROVal2", attributes[1].ReadOnlyProperty);
+            Assert.Equal("NVal2", attributes[1].NamedProperty);
+            Assert.Equal("Val2", attributes[1].NamedField);
+        }
+
+        [Something("ROVal1", NamedProperty = "NVal1", NamedField = "Val1")]
+        [Something("ROVal2", NamedProperty = "NVal2", NamedField = "Val2")]
+        public class Character
+        {
+        }
+
+        [AttributeUsage(AttributeTargets.All, AllowMultiple = true)]
+        public class SomethingAttribute : Attribute
+        {
+            public string NamedField;
+
+            public SomethingAttribute(string readOnlyProperty)
+            {
+                ReadOnlyProperty = readOnlyProperty;
+            }
+
+            public string ReadOnlyProperty { get; private set; }
+            public string NamedProperty { get; set; }
+        }
+    }
+}
